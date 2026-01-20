f11f6395752262766a1be11e6054d8f8530c93ba
Yassir Lazreq <yassirlazre@gmail.com>
2026-01-20 14:41:57 +0100

    Update database connection settings to include port in DSN and add port configuration

--- config/config.php
+++ config/config.php
@@ -8,6 +8,7 @@ return [
     ],
     'database' => [
         'host' => 'localhost',
+        'port' => '3307',
         'name' => 'jobdating',
         'username' => 'root',
         'password' => ''
